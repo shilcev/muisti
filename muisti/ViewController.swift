b51a937ded969d6a12c6591a8b01c7a498cde5f3
evgenii shiltsev <e.shiltsev@gmail.com>
2020-03-28 15:04:41 +0200

    Initial Commit

---
+++ muisti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  muisti
+//
+//  Created by Evgenii Shiltsev on 28.3.2020.
+//  Copyright © 2020 Evgenii Shiltsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
